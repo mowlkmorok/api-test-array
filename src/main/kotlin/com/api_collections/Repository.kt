fe9defe2cf28da25489bce3f5f8c59235d7430c4
mowlkmorok <65051191+mowlkmorok@users.noreply.github.com>
2021-07-11 20:40:38 -0300

    Adding some comments

--- src/main/kotlin/com/api_collections/Repository.kt
+++ src/main/kotlin/com/api_collections/Repository.kt
@@ -1,12 +1,17 @@
 package com.api_collections
 
 class Repository<T> {
+    //Simulation of a data base
+    //------------------------------------------
+    // Here we did create a mutable map
     private val map = mutableMapOf<String, T>()
-
+    
+    // Here we did create a function 'create'
     fun create(id: String, value: T){
         map[id] = value
     }
-
+    
+    // Here we put the id as return
     fun findById(id: String) = map[id]
 
 }
